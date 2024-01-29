41d83358ad76b9eacc4fd465a7c023a5d24ecd6d
officialalkenes <belloabdulhakeemolamide@gmail.com>
2024-01-29 17:33:43 +0100

    chore: base notification setup

---
+++ main.py
@@ -0,0 +1,31 @@
+from typing import Union
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+
+from connection import ConnectionManager
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+manager = ConnectionManager()
+
+# WebSocket endpoint
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_notification(f"Notification: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
